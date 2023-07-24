0379373b6305394470db08bd71f5aecab54070fe
Test Account <atest5992@gmail.com>
2023-07-24 13:50:30 +0300

    update project structure

--- src/main/java/com/example/studentapinodb/StudentApiNoDbApplication.java
+++ src/main/java/com/example/studentapinodb/StudentApiNoDbApplication.java
@@ -2,12 +2,12 @@ package com.example.studentapinodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class StudentApiNoDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentApiNoDbApplication.class, args);
     }
-
 }
